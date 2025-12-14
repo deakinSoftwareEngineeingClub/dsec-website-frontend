f536c0c6d00b3beeeeb499943cd16cd4e67610eb
Clupai8o0 <clupaio4@gmail.com>
2025-12-14 15:55:10 +1100

    made version 1 of the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,26 +1,43 @@
 "use client";
 
-import { Menu } from 'lucide-react';
+import { Menu } from "lucide-react";
 
 import Image from "next/image";
+import { AnimatedThemeToggler } from "./ui/animated-theme-toggler";
+
+//todo: add actual animated links
+//todo: add proper styling for the links
 
 export default function Navbar() {
 	return (
-        <div className={"fixed px-[20px] md:px-[80px] lg:px-[160px] left-0 top-0 w-full h-[48px] md:h-[72px] flex items-center justify-between bg-background z-50"}>
-            <Image className={"w-[42px] md:w-[72px] cursor-pointer"} width={983} height={612}  src="/logo.png" alt="DSEC logo"/>
+		<div className={"w-full flex bg-background h-20 border-b border-white/20"}>
+			<div className="max-w-7xl px-4 md:px-8 lg:px-0 w-full flex items-center justify-between mx-auto">
+				{/* //TODO: home link */}
+				<Image
+					className="md:h-[47.33px] md:w-19"
+					width={64.23}
+					height={40}
+					src="/logo.png"
+					alt="DSEC logo"
+				/>
 
-            {/*this is shown if the screen is large enough else hidden */}
-            <ul className={"gap-8 items-center hidden lg:flex w-fit p-1 px-6 bg-primary rounded-2xl"}>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Membership</li>
-                <li>Recruitment</li>
-                <li>Contact Us</li>
-            </ul>
+				{/*this is shown if the screen is large enough else hidden */}
+				<ul className="gap-8 items-center hidden lg:flex w-fit font-sans rounded-2xl">
+					<li>Home</li>
+					<li>About Us</li>
+					<li>Projects</li>
+					<li>Events</li>
+					<li>Contact Us</li>
+				</ul>
 
-            {/*hamburger for phones / small screens :)*/}
-            <Menu className={"max-w-[42px] lg:hidden cursor-pointer"} />
-        </div>
+				<ul className="gap-8 items-center hidden lg:flex w-fit font-sans rounded-2xl">
+					<li className="flex items-center"><AnimatedThemeToggler /></li>
+					<li><a href="/join-us" className="bg-primary rounded-full px-4 py-2">Join Now</a></li>
+				</ul>
+
+				{/*hamburger for phones / small screens :)*/}
+				<Menu className={"lg:hidden cursor-pointer"} size="32" />
+			</div>
+		</div>
 	);
-};
-
+}
